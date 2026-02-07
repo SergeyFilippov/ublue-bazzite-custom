a1c248a37321c39291d3f8de49b74529df3ee940
Sergey Filippov <sergey.filippov@outlook.com>
2026-02-07 12:42:35 +0100

    [build] code removed; keepasxc added

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,16 +9,11 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# remove packages
+dnf5 remove -y code
+
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y keepassxc
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Prepa for future upgrades
+# dnf5 versionlock add gamescope
